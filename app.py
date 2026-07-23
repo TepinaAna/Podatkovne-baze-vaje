370fccbb6922844d645b70c1505f488737067ef3
TepinaAna <ana.tepina.02@gmail.com>
2026-07-23 16:15:55 +0200

    Dodana povezava spletne aplikacije z bazo

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import sqlite3
 from flask import Flask
 
 app = Flask(__name__)
+
+def connect():
+    conn = sqlite3.connect("baza.sqlite")
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route("/")
 def index():
@@ -8,3 +14,4 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
